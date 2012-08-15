4de0d576c2ff32cff1a30cfdbd246eb30f34da8c
mabin359 <mabin359@gmail.com>
2012-08-15 17:10:17 +0900

    Init & Test

---
+++ Eclipse/FingerTracer-Linux/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 15, 2012
+ *      Author: mabin359
+ */
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello World" << endl;
+
+	return 0;
+}
