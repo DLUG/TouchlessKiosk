9c4275cb98da1bd4edc952acd99f0575ae193c62
mabin359 <mabin359@gmail.com>
2012-11-21 13:38:09 +0900

    - maybe miss file?

---
+++ Eclipse/Contents/WebContent/js/common.js
@@ -0,0 +1,8 @@
+//11.15 add taehwan
+$(document).keydown(function(event) {
+	if(event.whichevent.which == 72) {
+		location.href="/kiosk/";
+	} else (event.whichevent.which == 77){
+		location.href="/kiosk/main.htm";
+	}
+});
